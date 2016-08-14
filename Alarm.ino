2dc7120fdbb052ad4d45efc08a9f56ba4c86b266
nashanator17 <nashua_luk_17@hotmail.com>
2016-08-13 23:03:35 -0400

    Add files via upload

---
+++ Alarm.ino
@@ -0,0 +1,58 @@
+#include <Time.h>
+#include <TimeLib.h>
+#define TIME_MSG_LEN  11   // time sync to PC is HEADER followed by Unix time_t as ten ASCII digits
+#define TIME_HEADER  'T'   // Header tag for serial time sync message
+#define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
+
+
+#include <Servo.h>
+
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalClockDisplay();    //displays on serial monitor current time
+    delay(1000);
+
+  if (hour() == 7 && minute() == 30 && second() == 00){    //servo rotates when time is 7:30
+    myservo.write(180);
+  }
+}
+
+  void digitalClockDisplay(){
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year()); 
+  Serial.println(); 
+}
+
+  void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+/*
+ http://playground.arduino.cc/Code/Time
+ https://www.arduino.cc/en/Tutorial/Sweep
+
+*/
+
